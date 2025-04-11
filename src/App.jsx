9b4f762b78998c5409e4e1c847afde56e526291b
aitegin <aiteginsamudinov6@gmail.com>
2025-04-11 12:09:57 +0600

    fixed app jsx and theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,5 @@
-import { Typography } from '@mui/material'
-import { Themes } from './components/Themes';
-import React from 'react'
+import './App.css'
 
-export default function App() {
-   return (
-      <div>
-         <Themes>
-            <Typography variant="h1">H1 Ubuntu Bold</Typography>
-            <Typography variant="h2">H2 Inter Medium</Typography>
+const App = () => <h1>GADGETARIUM js-16</h1>
 
-            <Typography variant="h2" fontWeight={700}>
-               H2 Inter Bold
-            </Typography>
-
-            <Typography variant="h4" className="MuiTypography-h4-regular">
-               H4 Regular
-            </Typography>
-            <Typography variant="h4" className="MuiTypography-h4-medium">
-               H4 Medium
-            </Typography>
-
-            <Typography sx={{ fontWeight: 300, fontSize: '16px' }}>
-               Body Light
-            </Typography>
-            <Typography variant="bodyMedium">Body Medium (10px)</Typography>
-         </Themes>
-      </div>
-   )
-}
+export default App
