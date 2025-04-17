59350270978fb5335c656e95e534d775101d9a3e
aitegin <aiteginsamudinov6@gmail.com>
2025-04-17 18:28:09 +0600

    fixed App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
-// import './App.css'
+import './App.css'
 
-// const App = () => <h1>GADGETARIUM js-16</h1>
-
-// export default App
-
-import React from 'react'
-import Footer from './components/footer'
-
-function App() {
-   return (
-      <div>
-         <Footer />
-      </div>
-   )
-}
+const App = () => <h1>GADGETARIUM js-16</h1>
 
 export default App
+
+
