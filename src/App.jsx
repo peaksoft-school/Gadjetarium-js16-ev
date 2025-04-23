53181eec096e14bef47ad1ef49314a9c70a52b8e
Syimuk S. <raftasher2@gmail.com>
2025-04-23 14:02:17 +0600

    bugfix-clear-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,6 @@
-// import './App.css'
+import './App.css'
 
-// const App = () => <h1>GADGETARIUM js-16</h1>
-
-// export default App
-
-import ImageSlider from './components/ImageSlider'
-
-function App() {
-   return (
-      <div>
-         <ImageSlider />
-      </div>
-   )
-}
+const App = () => <h1>GADGETARIUM js-16</h1>
 
 export default App
+
