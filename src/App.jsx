6a31bb71f3d523ccc02343674ca2eb92eb7f8aa0
aitegin <aiteginsamudinov6@gmail.com>
2025-05-01 17:00:50 +0600

    fixed-App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,4 @@
 
 // export default App
 
-import React from 'react'
-import FAQ from './components/FAQ'
 
-const App = () => {
-   return (
-      <div>
-         <FAQ />
-      </div>
-   )
-}
-
-export default App
