1913631f2e24bda43dc2346acb72b7b04cba96e1
Syimuk S. <raftasher2@gmail.com>
2025-05-02 11:07:45 +0600

    fixed name of function

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import './App.css'
+// import './App.css'
 
-const App = () => <h1>GADGETARIUM js-16</h1>
+// const App = () => <h1>GADGETARIUM js-16</h1>
 
-export default App
+// export default App
+
