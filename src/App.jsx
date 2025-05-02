5fcf1729bf60b45cf6c3e7c54abd5dca2945e390
aitegin <aiteginsamudinov6@gmail.com>
2025-05-02 11:53:59 +0600

    fixed-app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,6 @@
-// import './App.css'
+import './App.css'
 
-// const App = () => <h1>GADGETARIUM js-16</h1>
-
-// export default App
-
-import Layout from './components/UI/Layout'
-
-function App() {
-   return (
-      <div>
-         <Layout />
-      </div>
-   )
-}
+const App = () => <h1>GADGETARIUM js-16</h1>
 
 export default App
+
