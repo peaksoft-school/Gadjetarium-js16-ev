eca712b93ded01ff80ff20feb07fae63b39f6ce5
aitegin <aiteginsamudinov6@gmail.com>
2025-05-05 11:57:20 +0600

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,5 @@
-// import './App.css'
+import './App.css'
 
-// const App = () => <h1>GADGETARIUM js-16</h1>
-
-// export default App
-
-import React from 'react'
-import PopUp from './components/UI/PopUp'
-
-const App = () => {
-   return (
-      <div>
-         <PopUp/>
-      </div>
-   )
-}
+const App = () => <h1>GADGETARIUM js-16</h1>
 
 export default App
