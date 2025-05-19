343d7036dba7e97152550e2bfbae4c8dee73a1b0
aitegin <aiteginsamudinov6@gmail.com>
2025-05-19 17:55:54 +0600

    started

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
-import './App.css'
+// import './App.css'
 
-const App = () => <h1>GADGETARIUM js-16</h1>
+// const App = () => <h1>GADGETARIUM js-16</h1>
+
+// export default App
+
+import React from 'react'
+import CustomerReviews from './pages/CustomerReviews'
+
+const App = () => {
+   return (
+      <div>
+         <CustomerReviews />
+      </div>
+   )
+}
 
 export default App
