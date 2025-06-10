2afa60803563fe3e1cdb20e1a31bd68b5a1c7126
aitegin <aiteginsamudinov6@gmail.com>
2025-06-10 13:52:40 +0600

    fixed

--- src/configs/axiosInstans.js
+++ src/configs/axiosInstans.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://10.10.11.156:8015';
+const BASE_URL = 'http://3.147.74.119';
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
