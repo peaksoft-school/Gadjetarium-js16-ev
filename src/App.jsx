3210c31f3cef738e6b146d2064f27f0d3a3cc2d9
aitegin <aiteginsamudinov6@gmail.com>
2025-06-28 18:02:21 +0600

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
-import './App.css'
-import AppRouter from './routes/AppRouter'
+// import './App.css'
+// import AppRouter from './routes/AppRouter'
 
-const App = () => <AppRouter />
+// const App = () => <AppRouter />
+
+// export default App
+
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
 
 export default App
