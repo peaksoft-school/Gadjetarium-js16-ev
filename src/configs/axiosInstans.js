a88a165c2011db25cf804d39d064c4f35913d62a
Syimuk S. <raftasher2@gmail.com>
2025-07-01 20:27:29 +0600

    .final save

--- src/configs/axiosInstans.js
+++ src/configs/axiosInstans.js
@@ -22,7 +22,7 @@ axiosInstance.interceptors.request.use(
       // const token = customStore?.getState()?.auth?.token || null
 
       const token =
-         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFpYmVrQGdtYWlsLmNvbSIsImlkIjo1LCJyb2xlIjoiQURNSU4iLCJpYXQiOjE3NTEzNTEwMDksImV4cCI6MTc1MTM1NDcwOX0.ybsuiPQmxQv3FxOvhyJ0TxZ831EEhBghQ7WiyDp1-GU'
+         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFpYmVrQGdtYWlsLmNvbSIsImlkIjo1LCJyb2xlIjoiQURNSU4iLCJpYXQiOjE3NTEzNzkzMzksImV4cCI6MTc1MTM4MzAzOX0.sLNq5bhhqPZNVz-iJpZRKTVSaDqPhrAyZpQ_M5JLMcE'
 
       if (token) {
          updateConfig.headers.Authorization = `Bearer ${token}`
