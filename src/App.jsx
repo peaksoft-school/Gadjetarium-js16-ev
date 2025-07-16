dd2140fbc0ba5f156ed3e635dadc08e568ba8602
aitegin <aiteginsamudinov6@gmail.com>
2025-07-16 10:32:44 +0600

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
-// import './App.css'
-// import AppRouter from './routes/AppRouter'
+import './App.css'
+import AppRouter from './routes/AppRouter'
 
-// const App = () => <AppRouter />
+const App = () => <AppRouter />
 
-// export default App
+export default App
 
-import React from 'react'
-import PaymentPage from './pages/PaymentPage'
 
-export default function App() {
-   return (
-      <div>
-         <PaymentPage />
-      </div>
-   )
-}
